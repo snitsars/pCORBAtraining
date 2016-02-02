0e4fbc72981b7d55f87ec364a4a70026e09ec59c
snitsars <sergsnicarenko@gmail.com>
2016-02-02 12:32:39 +0200

    Initial
    First  initial checkin

---
+++ Clients/Sharp/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting;
+using Ch.Elca.Iiop;
+
+namespace Org.Uneta.Iiopnet.Examples.First
+{
+    class FirstClient
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            try
+            {
+                // Адрес CORBA-сервера.
+                const string serverHost = "localhost";
+                const int serverPort = 1234;
+
+                // Запрашиваем имя пользователя.
+                Console.WriteLine("Enter your name please: ");
+                string userName = Console.ReadLine();
+
+                // Регистрируем канал IIOP.
+                IiopClientChannel channel = new IiopClientChannel();
+                ChannelServices.RegisterChannel(channel);
+
+                // Адрес CORBA-сервера.
+                string addressString = "iiop://localhost:1234/hello";
+                // Получаем ссылку на клиентский прокси.
+                IHello hello = (IHello)RemotingServices.Connect(typeof(IHello), addressString);
+                
+                // Вызываем CORBA-метод.
+                string serverResponse = hello.SayHello(userName);
+                serverResponse = hello.AddVAlue(5,10);
+                // Выводим ответ.
+                Console.WriteLine("Server ansver is: " + serverResponse);
+                Console.ReadLine();
+            } 
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception was raised: " + e);
+            }
+        }
+    }
+}
+
---
+++ Servers/Sharp/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels;
+using System.Threading;
+using Ch.Elca.Iiop;
+
+namespace Org.Uneta.Iiopnet.Examples.First
+{
+    public class HelloImplementation : MarshalByRefObject, IHello
+    {
+        public override object InitializeLifetimeService()
+        {
+            // Жизнь не кончается.
+            return null;
+        }
+
+        public string AddVAlue(int a, int b)
+        {
+            return Convert.ToString(a + b);
+        }
+
+        public string SayHello(string name)
+        {
+            return "Hello by CORBA, " + name + ".";
+        }
+
+    }
+
+    public class FirstServer
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            // Регистрируем серверный канал IIOP.
+            int serverPort = 1234;
+            IiopChannel channel = new IiopChannel(serverPort);
+#pragma warning disable CS0618 // Type or member is obsolete
+            ChannelServices.RegisterChannel(channel);
+#pragma warning restore CS0618 // Type or member is obsolete
+
+            // Создаем реализацию интерфейса IHello и публикуем её.
+            HelloImplementation helloImplementation = new HelloImplementation();
+            string objectURI = "hello";
+            RemotingServices.Marshal(helloImplementation, objectURI);
+
+            Console.WriteLine("Server ready to use.");
+            Thread.Sleep(Timeout.Infinite);
+        }
+    }
+}
