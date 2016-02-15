3aa545d691d963238720eff3ead06853fbef8b33
snitsars <sergsnicarenko@gmail.com>
2016-02-15 13:44:00 +0200

    Changes to be committed: 	modified:   config/prepareDebugEnv.bat

--- config/prepareDebugEnv.bat
+++ config/prepareDebugEnv.bat
@@ -7,4 +7,4 @@ mklink /H ..\build\Debug_Win32\omniORB421_vc14_rtd.pdb ..\thirdparty\omniORB-4.2
 mklink /H ..\build\Debug_Win32\omniDynamic421_vc14_rtd.pdb ..\thirdparty\omniORB-4.2.1\bin\x86_win32\omniDynamic421_vc14_rtd.pdb
 mklink /H ..\build\Debug_Win32\omnithread40_vc14_rtd.pdb ..\thirdparty\omniORB-4.2.1\bin\x86_win32\omnithread40_vc14_rtd.pdb
 
-SET OMNIORB_CONFIG="%~dp0\omniORB.cfg"
+SET OMNIORB_CONFIG="%~dp0omniORB.cfg"
