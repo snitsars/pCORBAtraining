06904d1b71f2de921152e2f486d97b9bad4b8b9b
snitsars <sergsnicarenko@gmail.com>
2016-02-15 13:39:35 +0200

    fix path to dll

--- config/prepareDebugEnv.bat
+++ config/prepareDebugEnv.bat
@@ -1,9 +1,10 @@
-mklink /H ..\build\Debug_Win32\omniORB421_vc14_rtd.dll ..\thirdparty\omniORB-4.2.1\src\lib\omniORB\orbcore\shareddebug\omniORB421_vc14_rtd.dll
-mklink /H ..\build\Debug_Win32\omniDynamic421_vc14_rtd.dll ..\thirdparty\omniORB-4.2.1\src\lib\omniORB\dynamic\shareddebug\omniDynamic421_vc14_rtd.dll
-mklink /H ..\build\Debug_Win32\omnithread40_vc14_rtd.dll ..\thirdparty\omniORB-4.2.1\src\lib\omnithread\shareddebug\omnithread40_vc14_rtd.dll
+cd %~dp0
+mklink /H ..\build\Debug_Win32\omniORB421_vc14_rtd.dll ..\thirdparty\omniORB-4.2.1\bin\x86_win32\omniORB421_vc14_rtd.dll
+mklink /H ..\build\Debug_Win32\omniDynamic421_vc14_rtd.dll ..\thirdparty\omniORB-4.2.1\bin\x86_win32\omniDynamic421_vc14_rtd.dll
+mklink /H ..\build\Debug_Win32\omnithread40_vc14_rtd.dll ..\thirdparty\omniORB-4.2.1\bin\x86_win32\omnithread40_vc14_rtd.dll
 
-mklink /H ..\build\Debug_Win32\omniORB421_vc14_rtd.pdb ..\thirdparty\omniORB-4.2.1\src\lib\omniORB\orbcore\shareddebug\omniORB421_vc14_rtd.pdb
-mklink /H ..\build\Debug_Win32\omniDynamic421_vc14_rtd.pdb ..\thirdparty\omniORB-4.2.1\src\lib\omniORB\dynamic\shareddebug\omniDynamic421_vc14_rtd.pdb
-mklink /H ..\build\Debug_Win32\omnithread40_vc14_rtd.pdb ..\thirdparty\omniORB-4.2.1\src\lib\omnithread\shareddebug\omnithread40_vc14_rtd.pdb
+mklink /H ..\build\Debug_Win32\omniORB421_vc14_rtd.pdb ..\thirdparty\omniORB-4.2.1\bin\x86_win32\omniORB421_vc14_rtd.pdb
+mklink /H ..\build\Debug_Win32\omniDynamic421_vc14_rtd.pdb ..\thirdparty\omniORB-4.2.1\bin\x86_win32\omniDynamic421_vc14_rtd.pdb
+mklink /H ..\build\Debug_Win32\omnithread40_vc14_rtd.pdb ..\thirdparty\omniORB-4.2.1\bin\x86_win32\omnithread40_vc14_rtd.pdb
 
-SET OMNIORB_CONFIG="F:\Serg\ALL_PROJECT\pCORBAtraining\config\omniORB.cfg"
+SET OMNIORB_CONFIG="%~dp0\omniORB.cfg"
